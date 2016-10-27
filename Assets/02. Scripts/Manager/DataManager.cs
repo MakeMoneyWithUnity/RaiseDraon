0b01482e2bd57c93e172d8635664a96cc547e27b
NakOh <kk0704@naver.com>
2016-10-27 23:56:27 +0900

    #기본 틀 제조

---
+++ Assets/02. Scripts/Manager/DataManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataManager : MonoBehaviour {
+    private int gold;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    public int Gold {
+        get {
+            return gold;
+        }
+
+        set {
+            gold = value;
+            PlayerPrefs.SetInt("Gold", gold);
+        }
+    }
+
+}
---
+++ Assets/02. Scripts/Manager/EventManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    //먹이주기
+    public void Eat() {
+
+    }
+
+    //청소하기
+    public void Clean() {
+
+    }
+
+    //공부하기
+    public void Study() {
+
+    }
+
+    //돈벌기
+    public void MakeMoney() {
+    }
+}
